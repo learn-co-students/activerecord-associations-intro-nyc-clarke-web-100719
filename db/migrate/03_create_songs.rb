53f2087e8ea1636e7a3dd0264f77db512739395e
soufianeou <soufiane.oucherrou@gmail.com>
2019-10-17 12:21:51 -0400

    Done.

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,10 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+
+    def change 
+        create_table :songs do |ele|
+            ele.string :name
+            ele.integer :artist_id #represent artist class or table
+            ele.integer :genre_id #represent genre class or table
+        end
+    end
 end
